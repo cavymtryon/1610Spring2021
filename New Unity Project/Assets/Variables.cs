762eb6760b87066caee561965c35e5d2d3310697
Cavy Melissa Tryon <10881022@my.uvu.edu>
2021-02-10 11:45:41 -0700

    Variables changes

--- New Unity Project/Assets/Variables.cs
+++ New Unity Project/Assets/Variables.cs
@@ -1,11 +1,15 @@
 int myNum = 50;
+
 string myName = "John";
+
 int x = 5;
 int y =10;
 Console.WriteLine(x+y);
+
 int x = 5;
 int y =10;
 int z = x+y;
 Console.WriteLine(z);
+
 int x = 5, y = 6, z = 50;
 Console.WriteLine( x + y + z);
