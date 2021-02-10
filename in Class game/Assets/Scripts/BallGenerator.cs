a47421dc8c01ad76019aeb2173d53c727c8615ab
Cavy Melissa Tryon <10881022@my.uvu.edu>
2021-02-10 13:07:54 -0700

    Ball Generator

---
+++ in Class game/Assets/Scripts/BallGenerator.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallGenerator : MonoBehaviour
+{
+    public GameObject ball;
+    private int number = 0;
+
+    IEnumerator Start()
+    {
+        while(number < 25)
+        {
+            yield return new WaitForSeconds(0.5f);
+            Instantiate(ball);
+            number++;
+        }
+
+    }
+    
+}
