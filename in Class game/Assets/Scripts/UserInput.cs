b8f1ff0f7dcaac1496aa7ee8b50a6d90a6a71c01
Cavy Melissa Tryon <10881022@my.uvu.edu>
2021-02-17 13:36:03 -0700

    Scripting

---
+++ in Class game/Assets/Scripts/UserInput.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UserInput : MonoBehaviour
+{
+    public Rigidbody2D ballRigidbody2D;
+    public Vector2 direction;
+    public Vector2 ydirection;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            ballRigidbody2D.AddForce(direction, ForceMode2D.Force);
+
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            ballRigidbody2D.AddForce(-direction, ForceMode2D.Force);
+        }
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            ballRigidbody2D.AddForce(ydirection, ForceMode2D.Force);
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            ballRigidbody2D.AddForce(-ydirection,ForceMode2D.Force);
+        }
+    }
+}
