dbcd8c133d0cbcd21abdeb1e7df33504f28d8c73
Cavy Melissa Tryon <10881022@my.uvu.edu>
2021-02-18 14:21:21 -0700

    Switch Script

---
+++ Switch.cs
@@ -0,0 +1,28 @@
+int day = 2;
+switch(day)
+{
+  case 1:
+    Console.WriteLine("Monday");
+    break;
+  case 2:
+    Console.WriteLine("Tuesday");
+    break;
+  case 3:
+    Console.WriteLine("Wednesday");
+    break;
+}
+
+int day = 3;
+switch(day)
+{
+    case 1:
+        Console.WriteLine("Today is saturday");
+        break;
+    case 2:
+        Console.WriteLine("Today is Sunday");
+        break;
+    default:
+        Console.WriteLine("Looking forward to the weekend");
+        break;
+
+}
